53b4d77c9ea56e57d27dc86476cdcef18a6d4ca7
sauravApollo <saurav@apollofinvest.com>
2020-11-28 14:02:26 +0530

    Minor: Cart count UI

--- src/commonComponents/Navbar/Navbar.js
+++ src/commonComponents/Navbar/Navbar.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Logo } from '../../SvgComponents'
-import { dummyProfilePic } from '../../imports'
-import './Navbar.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Logo } from "../../SvgComponents";
+import { dummyProfilePic } from "../../imports";
+import "./Navbar.scss";
 
 const Navbar = (props) => {
-    const { fromCart } = props
-    return (
-        <div className={[fromCart && 'cart-nav'].join(' ')}>
-            <div className="container">
-                <div className="d-flex flex-space-between pt-1 pb-1">
-                    <div className="d-flex flex-align-center">
-                        <Logo width={'36px'} height={'36px'} /> <span className="logo ml-0dot5">Happay</span>
-                    </div>
-                    <div className="d-flex flex-align-center">
-                        {
-                            !fromCart && (
-                                <Link to="/cart">
-                                    <i className="fa fa-shopping-cart cart-icon"></i>
-                                </Link>
-                            )
-                        }
-                        <img src={dummyProfilePic} alt="profile-pic" className="avatar ml-2" />
-                    </div>
+  const { fromCart, localData } = props;
+  return (
+    <div className={[fromCart && "cart-nav"].join(" ")}>
+      <div className="container">
+        <div className="d-flex flex-space-between pt-1 pb-1">
+          <div className="d-flex flex-align-center">
+            <Logo width={"36px"} height={"36px"} />{" "}
+            <span className="logo ml-0dot5">Happay</span>
+          </div>
+          <div className="d-flex flex-align-center">
+            {!fromCart && (
+              <Link to="/cart">
+                <div className="cart-icon-container">
+                  <div className="count-items d-flex flex-center">
+                    {localData.reduce((acc, cv) => {
+                      acc = acc + cv.quantity;
+                      return acc;
+                    }, 0)}
+                  </div>
+                  <i className="fa fa-shopping-cart cart-icon"></i>
                 </div>
-            </div>
+              </Link>
+            )}
+            <img
+              src={dummyProfilePic}
+              alt="profile-pic"
+              className="avatar ml-2"
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+const mapStateToProps = (state) => {
+  const { loadFromLocal } = state;
+  return { ...loadFromLocal };
+};
+
+export default connect(mapStateToProps, {})(Navbar);
