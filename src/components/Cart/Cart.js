6dc60ef4faea02a11ab9af0f8d91ebef54a488ba
sauravApollo <saurav@apollofinvest.com>
2020-11-28 14:10:11 +0530

    Minor: Hooked redux data to Cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,35 +1,13 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { Navbar } from "../../commonComponents";
 import { UIButton } from "../../UIComponents";
 import "../../utils";
 import "./Cart.scss";
 
-const data = [
-  {
-    id: 1,
-    name: "food card",
-    price: 21,
-    original_price: 30,
-    quantity: 2,
-  },
-  {
-    id: 3,
-    name: "epic card",
-    price: 40,
-    original_price: 40,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    name: "travel card",
-    price: 20,
-    original_price: 20,
-    quantity: 1,
-  },
-];
-
-const Cart = () => {
+const Cart = (props) => {
+  const { localData } = props;
   return (
     <Fragment>
       <Navbar fromCart={true} />
@@ -43,7 +21,9 @@ const Cart = () => {
               </Link>
             </div>
           </div>
-          <div className="mt-2 title-text">Order Summary ({data.length})</div>
+          <div className="mt-2 title-text">
+            Order Summary ({localData.length})
+          </div>
           <div className="mt-2 d-flex flex-space-between flex-wrap mb-4">
             <div className="d-flex flex-column col-1-container">
               <div className="p-2">
@@ -53,16 +33,21 @@ const Cart = () => {
                   <div className="table-heading">QTY</div>
                 </div>
                 <div className="border-bottom pl-1 pr-1 mb-2">
-                  {data.map((ele, id) => (
-                    <div
-                      className="d-flex flex-space-between mb-2"
-                      key={ele.id}
-                    >
-                      <div className="table-data">{id + 1}</div>
-                      <div className="table-data">{ele.name.toTitleCase()}</div>
-                      <div className="table-data">{ele.quantity}</div>
-                    </div>
-                  ))}
+                  {localData.map(
+                    (ele, id) =>
+                      ele.quantity > 0 && (
+                        <div
+                          className="d-flex flex-space-between mb-2"
+                          key={ele.id}
+                        >
+                          <div className="table-data">{id + 1}</div>
+                          <div className="table-data">
+                            {ele.name.toTitleCase()}
+                          </div>
+                          <div className="table-data">{ele.quantity}</div>
+                        </div>
+                      )
+                  )}
                 </div>
                 <div className="add-more-text pl-1 mb-2">
                   <Link to="/">
@@ -77,21 +62,27 @@ const Cart = () => {
                   <div className="sub-title-text">Price Details</div>
                 </div>
                 <div className="border-bottom pl-1 pr-1 mb-2">
-                  {data.map((ele, id) => (
-                    <div className="d-flex flex-space-between mb-2" key={id}>
-                      <div className="">{`${ele.quantity} X $ ${ele.price}.00`}</div>
-                      <div className="">{`$ ${
-                        ele.quantity * ele.price
-                      }.00`}</div>
-                    </div>
-                  ))}
+                  {localData.map(
+                    (ele, id) =>
+                      ele.quantity > 0 && (
+                        <div
+                          className="d-flex flex-space-between mb-2"
+                          key={id}
+                        >
+                          <div className="">{`${ele.quantity} X $ ${ele.price}.00`}</div>
+                          <div className="">{`$ ${
+                            ele.quantity * ele.price
+                          }.00`}</div>
+                        </div>
+                      )
+                  )}
                 </div>
                 <div className="border-bottom pl-1 pr-1 mb-2">
                   <div className="d-flex flex-space-between mb-2">
                     <div className="">Total Savings</div>
                     <div className="green-text">
                       {"- $ "}
-                      {data.reduce((acc, currval) => {
+                      {localData.reduce((acc, currval) => {
                         acc =
                           acc +
                           (currval.original_price - currval.price) *
@@ -117,7 +108,7 @@ const Cart = () => {
                   <div className="sub-title-text">To Pay</div>
                   <div className="sub-title-price-text">
                     {"$ "}
-                    {data.reduce((acc, currval) => {
+                    {localData.reduce((acc, currval) => {
                       acc =
                         acc +
                         (currval.price * currval.quantity -
@@ -139,4 +130,9 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+const mapStateToProps = (state) => {
+  const { loadFromLocal } = state;
+  return { ...loadFromLocal };
+};
+
+export default connect(mapStateToProps, {})(Cart);
